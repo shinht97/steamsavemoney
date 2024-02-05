3374afc6855b00f8069ea59ec47cd33f48e79526
Doyun05 <Doyun05@gmail.com>
2024-02-05 16:50:30 +0900

    KJ

---
+++ steampp_app.py
@@ -0,0 +1,117 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+import pandas as pd
+from sklearn.metrics.pairwise import linear_kernel
+from gensim.models import Word2Vec
+from scipy.io import mmread
+from PyQt5.QtGui import *
+import pickle
+from PyQt5.QtCore import QStringListModel
+
+
+form_window = uic.loadUiType('./steampp.ui')[0]
+
+
+class Exam(QWidget, form_window):
+    def __init__(self):
+
+        super().__init__()
+
+        self.setupUi(self)
+
+        self.game_list = pd.read_csv("./steam.csv")
+
+        # 이미지 넣기---------------------------------------------------
+        self.label_2.setPixmap(QPixmap("./steam.jpg"))
+        self.label_2.setScaledContents(True)
+
+        self.label_3.setPixmap(QPixmap("./steam_2.jpg"))
+        self.label_3.setScaledContents(True)
+        # -------------------------------------------------------------
+        self.Tfidf_matrix = mmread('./models/Tfidf_review.mtx').tocsr()
+        with open('./models/tfidf.pickle', 'rb') as f:
+            self.Tfidf = pickle.load(f)
+        self.embedding_model = Word2Vec.load('./models/word2vec_reviews.model')
+        self.df_reviews = pd.read_csv('./cleaned_review.csv')
+        self.titles = list(self.df_reviews['titles'])
+        self.titles.sort()
+        for title in self.titles:
+            self.comboBox.addItem(title)
+
+        model = QStringListModel()
+        model.setStringList(self.titles)
+        completer = QCompleter()
+        completer.setModel(model)
+        self.le_keyword.setCompleter(completer)
+
+        self.comboBox.currentIndexChanged.connect(self.combobox_slot)
+        self.btn_recommendation.clicked.connect(self.btn_slot)
+
+
+    def btn_slot(self):
+        key_word = self.le_keyword.text()
+        if key_word in self.titles:
+            recommendation = self.recommendatio_by_movie_title(key_word)
+        else:
+            recommendation = self.recommendation_by_keyword(key_word)
+        if recommendation:
+            self.lbl_recommendation.setText(recommendation)
+
+
+
+    def combobox_slot(self):
+        title = self.comboBox.currentText()
+        print(title)
+        recommendation = self.recommendatio_by_movie_title(title)
+        print('debug01')
+        self.lbl_recommendation.setText(recommendation)
+        print('debug02')
+
+    def recommendation_by_keyword(self, key_word):
+        try:
+            sim_word = self.embedding_model.wv.most_similar(key_word, topn=10)
+        except:
+            self.lbl_recommendation.setText('그럼 게임 없어요...')
+            return 0
+        words = [key_word]
+        for word, _ in sim_word:
+            words.append(word)
+        setence = []
+        count = 10
+        for word in words:
+            setence = setence + [word] * count
+            count -= 1
+        setence = ' '.join(setence)
+        print(setence)
+        setence_vec = self.Tfidf.transform([setence])
+        cosine_sim = linear_kernel(setence_vec, self.Tfidf_matrix)
+        recommendation = self.getRecommendation(cosine_sim)
+        recommendation = '\n'.join(list(recommendation))
+        return recommendation
+
+
+    def recommendatio_by_movie_title(self, title):
+        movie_idx = self.df_reviews[self.df_reviews['titles'] == title].index
+        cosine_sim = linear_kernel(self.Tfidf_matrix[movie_idx], self.Tfidf_matrix)
+        recommendation = self.getRecommendation(cosine_sim)
+
+        recommendation = "\n".join(list(recommendation))
+
+        return recommendation
+
+    def getRecommendation(self, cosine_sim):
+        simScore = list(enumerate(cosine_sim[-1]))
+        simScore = sorted(simScore, key=lambda x: x[1], reverse=True)
+        simScore = simScore[:11]
+        movieIdx = [i[0] for i in simScore]
+        recmovieList = self.df_reviews.iloc[movieIdx, 0]
+
+        return recmovieList[1:]
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = Exam()
+    mainWindow.show()
+    sys.exit(app.exec_())
